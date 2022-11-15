c85745b6bdec80a706864bf5962c4b5774260e10
Xayw3 <xaywe1@gmail.com>
2022-11-15 20:17:41 +0200

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import Experience from './components/Experience/Experience';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import NavBar from './components/NavBar/NavBar';
-import Portfolio from './components/Portfolio/Portfolio';
+import LandingPage from './components/LandingPage/LandingPage';
 
-const App = () => (
-  <>
-    <Header />
-    <NavBar />
-    <About />
-    <Experience />
-    <Portfolio />
-    <Contact />
-    <Footer />
-  </>
-);
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [loading]);
+
+  return (
+    <>
+      {
+        loading ? <p>loading...</p> : <LandingPage />
+      }
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,21 @@
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Experience from '../Experience/Experience';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import NavBar from '../NavBar/NavBar';
+import Portfolio from '../Portfolio/Portfolio';
+
+const LandingPage = () => (
+  <>
+    <Header />
+    <NavBar />
+    <About />
+    <Experience />
+    <Portfolio />
+    <Contact />
+    <Footer />
+  </>
+);
+
+export default LandingPage;
